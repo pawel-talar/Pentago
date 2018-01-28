fb96354bc3813a0eaaa3e4af2babe8cd0f0821be
Paweł Talar <ptalar15@gmail.com>
2018-01-29 00:51:44 +0100

    Add first version of rotationLeft function, not tested!

--- logic.c
+++ logic.c
@@ -18,6 +18,68 @@ void choosenField(GtkWidget *widget, gpointer data)
     updateBoard();
 }
 
+static void rotateLeft(int quarter)
+{
+    int tab[boardSize+1][boardSize+1];
+    for(int i = 0; i < boardSize; i++)
+        for(int j = 0; j < boardSize; j++)
+            tab[i][j] = 0;
+    int i = 0;
+    int j = 0;
+    int n = 0;
+    int m = 0;
+    if(quarter == 1){
+        i = 0;
+        j = 0;
+        n = boardSize/2;
+        m = boardSize/2;
+    }
+    else if(quarter == 2)
+    {
+        i = 0;
+        j = boardSize/2;
+        n = boardSize/2;
+        m = boardSize;
+    }
+    else if(quarter == 3)
+    {
+        i = boardSize/2;
+        j = 0;
+        n = boardSize;
+        m = boardSize/2;
+    }
+    else if(quarter == 4)
+    {
+        i = boardSize/2;
+        j = boardSize/2;
+        n = boardSize;
+        m = boardSize;
+    }
+    int k = i;
+    int l = j;
+    for(; i < n; i++)
+    {
+        for(; j < m; j++)
+        {
+           // printf("%d %d\n", i, j);
+            tab[n-j-1][i] = gameBoard[i][j];
+            printf("%d %d\n", n-j-1, i);
+        }
+        j = l;
+    }
+    i = k;
+    j = l;
+    for(; i < n; i++)
+    {
+        for(; j < m; j++)
+        {
+           // printf("%d %d\n", k, l);
+            gameBoard[i][j] = tab[i][j];
+            printf("%d\n", tab[i][j]);
+        }
+        j = l;
+    }
+}
 
 void upQuartersRotating(GtkWidget *widget, gpointer data)
 {
@@ -26,27 +88,10 @@ void upQuartersRotating(GtkWidget *widget, gpointer data)
         printf("Najpierw wybierz pole!\n");
     else
     {
-        int tab[4][4];
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++)
-                tab[i][j] = 0;
         if(a == 0)
-        {
-            for(int i = 0; i < boardSize/2; i++)
-            {
-                for(int j = 0; j < boardSize/2; j++)
-                {
-                    tab[i][j] = gameBoard[j][i];
-                }
-            }
-            for(int i = 0; i < boardSize/2; i++)
-            {
-                for(int j = 0; j < boardSize/2; j++)
-                {
-                    gameBoard[i][j] = tab[i][j];
-                }
-            }
-        }
+            rotateLeft(1);
+        else if(a == (int *) 2)
+            rotateLeft(2);
     }
     updateBoard();
 }
@@ -56,4 +101,12 @@ void downQuartersRotating(GtkWidget *widget, gpointer data)
     int * a = data;
     if(moveStep == 1)
         printf("Najpierw wybierz pole!\n");
+    else
+    {
+        if(a == 0)
+            rotateLeft(3);
+        if(a == (int *) 2)
+            rotateLeft(4);
+    }
+    updateBoard();
 }
